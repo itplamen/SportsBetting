b381bae81eda14ff594b17ac23a7e4a523f169b7
itplamen@gmail.com <itplamen@gmail.com>
2019-05-04 16:37:12 +0300

    Created SportsBetting db context

---
+++ SportsBetting/Data/SportsBetting.Data/Contracts/ISportsBettingDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace SportsBetting.Data.Contracts
+{
+    using MongoDB.Driver;
+
+    using SportsBetting.Data.Models.Base;
+
+    public interface ISportsBettingDbContext
+    {
+        IMongoCollection<T> GetCollection<T>(string name)
+             where T : BaseModel;
+    }
+}
---
+++ SportsBetting/Data/SportsBetting.Data/SportsBettingDbContext.cs
@@ -0,0 +1,34 @@
+﻿namespace SportsBetting.Data
+{
+    using MongoDB.Driver;
+
+    using SportsBetting.Data.Contracts;
+    using SportsBetting.Data.Models.Base;
+
+    public class SportsBettingDbContext : ISportsBettingDbContext
+    {
+        private const string DATABASE_HOST = "mongodb://127.0.0.1";
+        private const string DATABASE_NAME = "SportsBetting";
+
+        private readonly IMongoDatabase database;
+
+        public SportsBettingDbContext()
+        {
+            this.database = GetDataBase();
+        }
+
+        public IMongoCollection<T> GetCollection<T>(string name)
+             where T : BaseModel
+        {
+            return this.database.GetCollection<T>(name);
+        }
+
+        private IMongoDatabase GetDataBase()
+        {
+            IMongoClient mongoClient = new MongoClient(DATABASE_HOST);
+            IMongoDatabase database = mongoClient.GetDatabase(DATABASE_NAME);
+
+            return database;
+        }
+    }
+}
