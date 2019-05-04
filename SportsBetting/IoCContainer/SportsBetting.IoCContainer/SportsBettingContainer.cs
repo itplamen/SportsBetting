acc85b2d02ed71513508834309996acf68059f22
itplamen@gmail.com <itplamen@gmail.com>
2019-05-04 17:18:14 +0300

    Created SportsBetting container

---
+++ SportsBetting/IoCContainer/SportsBetting.IoCContainer/SportsBettingContainer.cs
@@ -0,0 +1,23 @@
+﻿namespace SportsBetting.IoCContainer
+{
+    using SimpleInjector;
+    using SimpleInjector.Packaging;
+
+    public static class SportsBettingContainer
+    {
+        private readonly static Container container = new Container();
+
+        public static void Initialize(IPackage package)
+        {
+            package.RegisterServices(container);
+
+            container.Verify();
+        }
+
+        public static T Resolve<T>()
+            where T : class
+        {
+            return container.GetInstance<T>();
+        }
+    }
+}
