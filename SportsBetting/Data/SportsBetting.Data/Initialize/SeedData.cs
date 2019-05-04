6550f0af90fd52f63c09ee7f6c33e3eeafb8ac01
itplamen@gmail.com <itplamen@gmail.com>
2019-05-04 16:49:24 +0300

    Added seed data

---
+++ SportsBetting/Data/SportsBetting.Data/Initialize/SeedData.cs
@@ -0,0 +1,30 @@
+﻿namespace SportsBetting.Data.Initialize
+{
+    using System;
+    using System.Collections.Generic;
+
+    using SportsBetting.Data.Models;
+
+    internal sealed class SeedData
+    {
+        internal SeedData()
+        {
+            this.Sports = new List<Sport>();
+            this.SeedSports();
+        }
+
+        internal ICollection<Sport> Sports { get; private set; }
+
+        private void SeedSports()
+        {
+            Sport sport = new Sport()
+            {
+                Key = 1,
+                Name = "eSports",
+                CreatedOn = DateTime.UtcNow
+            };
+
+            Sports.Add(sport);
+        }
+    }
+}
