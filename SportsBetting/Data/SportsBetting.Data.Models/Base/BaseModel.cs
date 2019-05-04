1d078a2be709b7a465695e7abb319b7c578576c9
itplamen@gmail.com <itplamen@gmail.com>
2019-05-04 15:30:29 +0300

    Created base data model and interfaces

---
+++ SportsBetting/Data/SportsBetting.Data.Models/Base/BaseModel.cs
@@ -0,0 +1,23 @@
+﻿namespace SportsBetting.Data.Models.Base
+{
+    using System;
+
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+
+    public abstract class BaseModel : IAuditInfo, IDeletableEntity
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public int Key { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+    }
+}
---
+++ SportsBetting/Data/SportsBetting.Data.Models/Base/IAuditInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace SportsBetting.Data.Models.Base
+{
+    using System;
+
+    public interface IAuditInfo
+    {
+        DateTime CreatedOn { get; set; }
+
+        DateTime? ModifiedOn { get; set; }
+    }
+}
