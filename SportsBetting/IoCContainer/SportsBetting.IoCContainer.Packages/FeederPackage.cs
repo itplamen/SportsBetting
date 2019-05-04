d96bafd35cb246c9b51195864d980c41c2eda9ae
itplamen@gmail.com <itplamen@gmail.com>
2019-05-05 00:29:56 +0300

    Created feeder IoC package and registered factories

---
+++ SportsBetting/IoCContainer/SportsBetting.IoCContainer.Packages/FeederPackage.cs
@@ -0,0 +1,22 @@
+﻿namespace SportsBetting.IoCContainer.Packages
+{
+    using SimpleInjector;
+    using SimpleInjector.Packaging;
+
+    using SportsBetting.Services.Feeder.Contracts.Factories;
+    using SportsBetting.Services.Feeder.Factories;
+
+    public sealed class FeederPackage : IPackage
+    {
+        public void RegisterServices(Container container)
+        {
+            RegisterFactories(container);
+        }
+
+        private void RegisterFactories(Container container)
+        {
+            container.Register<IWebDriverFactory, WebDriverFactory>(Lifestyle.Singleton);
+            container.Register<IObjectFactory, ObjectFactory>(Lifestyle.Singleton);
+        }
+    }
+}
