f3a3b6770535c19343967c4c52c264156ec13a3b
itplamen@gmail.com <itplamen@gmail.com>
2019-05-04 20:24:54 +0300

    Created object factory

---
+++ SportsBetting/Services/SportsBetting.Services.Feeder/Contracts/Factories/IObjectFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace SportsBetting.Services.Feeder.Contracts.Factories
+{
+    using System;
+    using System.Collections.Generic;
+
+    using SportsBetting.Feeder.Models;
+
+    public interface IObjectFactory
+    {
+        Market CreateMarket(string name, int matchId, IEnumerable<Odd> odds);
+
+        Match CreateMatch(string url, bool isLive, DateTime startTime, MatchStatus status, Team homeTeam, Team awayTeam, Tournament tournament, IEnumerable<Market> markets);
+
+        Odd CreateOdd(string name, decimal value, bool isSuspended, OddResultStatus resultStatus, int rank, int matketId, string header = null);
+
+        Team CreateTeam(string name, int? score);
+
+        Tournament CreateTournament(string name, string category);
+    }
+}
---
+++ SportsBetting/Services/SportsBetting.Services.Feeder/Factories/ObjectFactory.cs
@@ -0,0 +1,83 @@
+﻿namespace SportsBetting.Services.Feeder.Factories
+{
+    using System;
+    using System.Collections.Generic;
+
+    using SportsBetting.Feeder.Models;
+    using SportsBetting.Services.Feeder.Contracts.Factories;
+
+    public class ObjectFactory : IObjectFactory
+    {
+        public Market CreateMarket(string name, int matchId, IEnumerable<Odd> odds)
+        {
+            return new Market()
+            {
+                Name = name,
+                MatchId = matchId,
+                Odds = odds
+            };
+        }
+
+        public Match CreateMatch(
+            string url,
+            bool isLive,
+            DateTime startTime,
+            MatchStatus status,
+            Team homeTeam,
+            Team awayTeam,
+            Tournament tournament,
+            IEnumerable<Market> markets)
+        {
+            return new Match()
+            {
+                Url = url,
+                IsLive = isLive,
+                StartTime = startTime,
+                Status = status,
+                HomeTeam = homeTeam,
+                AwayTeam = awayTeam,
+                Tournament = tournament,
+                Markets = markets
+            };
+        }
+
+        public Odd CreateOdd(
+            string name, 
+            decimal value, 
+            bool isSuspended, 
+            OddResultStatus resultStatus, 
+            int rank, 
+            int matketId, 
+            string header = null)
+        {
+            return new Odd()
+            {
+                Name = name,
+                Value = value,
+                Header = header,
+                IsSuspended = isSuspended,
+                ResultStatus = resultStatus,
+                Rank = rank,
+                MarketId = matketId
+            };
+        }
+
+        public Team CreateTeam(string name, int? score)
+        {
+            return new Team()
+            {
+                Name = name,
+                Score = score
+            };
+        }
+
+        public Tournament CreateTournament(string name, string category)
+        {
+            return new Tournament()
+            {
+                Name = name,
+                Category = category
+            };
+        }
+    }
+}
