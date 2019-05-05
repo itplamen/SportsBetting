2541a0df44c7b99f8fe53b04996badaf43b1d379
itplamen@gmail.com <itplamen@gmail.com>
2019-05-05 15:21:14 +0300

    Created base odds provider

---
+++ SportsBetting/Services/SportsBetting.Services.Feeder/Providers/Odds/Base/BaseOddsProvider.cs
@@ -0,0 +1,59 @@
+﻿namespace SportsBetting.Services.Feeder.Providers.Odds.Base
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using HtmlAgilityPack;
+
+    using SportsBetting.Feeder.Common.XPaths;
+    using SportsBetting.Feeder.Models;
+    using SportsBetting.Services.Feeder.Contracts.Factories;
+    using SportsBetting.Services.Feeder.Contracts.Services;
+
+    public abstract class BaseOddsProvider
+    {
+        private readonly IHtmlService htmlService;
+        private readonly IObjectFactory objectFactory;
+
+        public BaseOddsProvider(IHtmlService htmlService, IObjectFactory objectFactory)
+        {
+            this.htmlService = htmlService;
+            this.objectFactory = objectFactory;
+        }
+
+        protected Odd BuildOdd(HtmlNode oddNode, string name, int rank, int marketId, string header = null)
+        {
+            decimal value = GetValue(oddNode);
+            bool isSuspended = IsSuspended(oddNode);
+            OddResultStatus resultStatus = htmlService.GetOddResultStatus(oddNode);
+
+            return objectFactory.CreateOdd(name, value, isSuspended, resultStatus, rank, marketId, header);
+        }
+
+        protected virtual decimal GetValue(HtmlNode oddNode)
+        {
+            decimal parsedValue = 0;
+
+            try
+            {
+                HtmlNodeCollection nodeCollection = oddNode.SelectNodes(OddXPaths.VALUE);
+
+                if (nodeCollection != null)
+                {
+                    string value = nodeCollection.Select(x => x.InnerText).First();
+                    decimal.TryParse(value, out parsedValue);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return parsedValue;
+        }
+
+        protected abstract bool ShouldGet(HtmlNode marketNode, IList<string> oddNames);
+
+        protected abstract bool IsSuspended(HtmlNode oddNode);
+    }
+}
