0e80c68505378fcc4f10bbbf75127427a5fe52d2
itplamen@gmail.com <itplamen@gmail.com>
2019-05-05 21:15:32 +0300

    Added match xpaths constants

--- SportsBetting/Feeder/SportsBetting.Feeder.Common/XPaths/MatchXPaths.cs
+++ SportsBetting/Feeder/SportsBetting.Feeder.Common/XPaths/MatchXPaths.cs
@@ -3,5 +3,7 @@
     public static class MatchXPaths
     {
         public const string MARKETS = ".//div[starts-with(@class,'__app-TableGroupMarket-table marketTable__table___dvHTz')]";
+
+        public const string HEADER_INFO_BOX = ".//div[starts-with(@class,'MatchHeaderInfobox')]";
     }
 }
