2c1ba0446251356abce042651c0652ad9209952e
itplamen@gmail.com <itplamen@gmail.com>
2019-05-05 15:46:08 +0300

    Created three-way odds provider

---
+++ SportsBetting/Services/SportsBetting.Services.Feeder/Providers/Odds/ThreeWayOddsProvider.cs
@@ -0,0 +1,67 @@
+﻿namespace SportsBetting.Services.Feeder.Providers.Odds
+{
+    using System.Collections.Generic;
+
+    using HtmlAgilityPack;
+
+    using SportsBetting.Feeder.Common.XPaths;
+    using SportsBetting.Feeder.Models;
+    using SportsBetting.Services.Feeder.Contracts.Factories;
+    using SportsBetting.Services.Feeder.Contracts.Providers.Odds;
+    using SportsBetting.Services.Feeder.Contracts.Services;
+    using SportsBetting.Services.Feeder.Providers.Odds.Base;
+
+    public class ThreeWayOddsProvider : BaseOddsProvider, IOddsProvider
+    {
+        private const int ODDS_COUNT = 3;
+
+        private readonly IHtmlService htmlService;
+        private readonly IOddsProvider oddsProvider;
+
+        public ThreeWayOddsProvider(IHtmlService htmlService, IObjectFactory objectFactory, IOddsProvider oddsProvider)
+            : base(htmlService, objectFactory)
+        {
+            this.htmlService = htmlService;
+            this.oddsProvider = oddsProvider;
+        }
+
+        public IEnumerable<Odd> Get(HtmlNode marketNode, IList<string> oddNames, int marketId)
+        {
+            if (ShouldGet(marketNode, oddNames))
+            {
+                ICollection<Odd> odds = new List<Odd>();
+                IList<HtmlNode> oddNodes = marketNode.SelectNodes(OddXPaths.NODE);
+
+                for (int i = 0; i < oddNames.Count; i++)
+                {
+                    Odd odd = BuildOdd(oddNodes[i], oddNames[i], i, marketId);
+                    odds.Add(odd);
+                }
+
+                return odds;
+            }
+
+            return oddsProvider.Get(marketNode, oddNames, marketId);
+        }
+
+        protected override bool ShouldGet(HtmlNode marketNode, IList<string> oddNames)
+        {
+            int oddsCount = htmlService.GetThreeWayOddsCount(marketNode);
+            bool isOddsCountValid = oddNames.Count == ODDS_COUNT && oddsCount == ODDS_COUNT;
+
+            return isOddsCountValid && !htmlService.HasHeader(marketNode);
+        }
+
+        protected override bool IsSuspended(HtmlNode oddNode)
+        {
+            return htmlService.IsSuspended(oddNode.FirstChild);
+        }
+
+        protected override decimal GetValue(HtmlNode oddNode)
+        {
+            decimal.TryParse(oddNode.LastChild.InnerText, out decimal value);
+
+            return value;
+        }
+    }
+}
