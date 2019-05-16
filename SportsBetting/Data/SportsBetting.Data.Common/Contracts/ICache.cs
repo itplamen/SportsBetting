227d040ab5a444d0076c689af5fcaa466d01bb5a
itplamen@gmail.com <itplamen@gmail.com>
2019-05-17 00:06:05 +0300

    Created ICache interface and decorator cache repository

---
+++ SportsBetting/Data/SportsBetting.Data.Common/Contracts/ICache.cs
@@ -0,0 +1,20 @@
+﻿namespace SportsBetting.Data.Common.Contracts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq.Expressions;
+
+    using SportsBetting.Data.Models.Base;
+
+    public interface ICache<TKey, TEntity>
+        where TEntity : BaseModel
+    {
+        IEnumerable<TEntity> All(Expression<Func<TEntity, bool>> filterExpression);
+
+        void Add(TKey key, TEntity entity);
+
+        void Delete(TKey key, TEntity entity);
+
+        void HardDelete(TKey key);
+    }
+}
