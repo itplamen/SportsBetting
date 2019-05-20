6295ce709b4c127329055d159b0fb6753ca288c8
itplamen@gmail.com <itplamen@gmail.com>
2019-05-20 23:03:00 +0300

    Created categories data cache

---
+++ SportsBetting/Data/SportsBetting.Data.Cache/CategoriesCache.cs
@@ -0,0 +1,28 @@
+﻿namespace SportsBetting.Data.Cache
+{
+    using System.Collections.Generic;
+
+    using SportsBetting.Data.Cache.General;
+    using SportsBetting.Data.Common.Contracts;
+    using SportsBetting.Data.Models;
+
+    public class CategoriesCache : BaseCache<int, Category>
+    {
+        private readonly IRepository<Category> categoriesRepository;
+
+        public CategoriesCache(IRepository<Category> categoriesRepository)
+        {
+            this.categoriesRepository = categoriesRepository;
+        }
+
+        public override void Load()
+        {
+            IEnumerable<Category> categories = categoriesRepository.All(x => !x.IsDeleted);
+
+            foreach (var category in categories)
+            {
+                Cache[category.Key] = category;
+            }
+        }
+    }
+}
