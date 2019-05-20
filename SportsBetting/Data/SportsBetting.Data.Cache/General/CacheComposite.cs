2fd89fcc2d84869cd41106bb5b6e9d643ea6874d
itplamen@gmail.com <itplamen@gmail.com>
2019-05-20 23:08:52 +0300

    Created data cache composite

---
+++ SportsBetting/Data/SportsBetting.Data.Cache/General/CacheComposite.cs
@@ -0,0 +1,22 @@
+﻿namespace SportsBetting.Data.Cache.General
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using SportsBetting.Data.Models.Base;
+
+    public class CacheComposite : BaseCache<int, BaseModel>
+    {
+        private readonly IEnumerable<BaseCache<int, BaseModel>> caches;
+
+        public CacheComposite(IEnumerable<BaseCache<int, BaseModel>> caches)
+        {
+            this.caches = caches;
+        }
+
+        public override void Load()
+        {
+            caches.ToList().ForEach(cache => cache.Load());
+        }
+    }
+}
