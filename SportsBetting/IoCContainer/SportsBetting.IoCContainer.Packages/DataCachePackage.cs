737e396fa2ef584008cebf404c3827c1f35346cc
itplamen@gmail.com <itplamen@gmail.com>
2019-05-22 22:24:49 +0300

    Created data cache package

---
+++ SportsBetting/IoCContainer/SportsBetting.IoCContainer.Packages/DataCachePackage.cs
@@ -0,0 +1,24 @@
+﻿namespace SportsBetting.IoCContainer.Packages
+{
+    using SimpleInjector;
+    using SimpleInjector.Packaging;
+
+    using SportsBetting.Data.Cache;
+    using SportsBetting.Data.Cache.General;
+    using SportsBetting.Data.Common;
+    using SportsBetting.Data.Common.Contracts;
+
+    public sealed class DataCachePackage : IPackage
+    {
+        public void RegisterServices(Container container)
+        {
+            container.Register(typeof(ICacheLoaderRepository<>), typeof(CacheLoaderRepository<>), Lifestyle.Singleton);
+
+            container.Collection.Register(typeof(ICache <,>),
+                typeof(CategoriesCache),
+                typeof(TeamsCache));
+
+            container.Register(typeof(ICache<,>), typeof(CacheComposite), Lifestyle.Singleton);
+        }
+    }
+}
