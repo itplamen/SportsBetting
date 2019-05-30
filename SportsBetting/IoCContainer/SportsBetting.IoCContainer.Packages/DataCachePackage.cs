261962a6c21d990a6f247eea22cd711c92082c66
itplamen@gmail.com <itplamen@gmail.com>
2019-05-30 23:26:48 +0300

    Registered the necessary dependencies

--- SportsBetting/IoCContainer/SportsBetting.IoCContainer.Packages/DataCachePackage.cs
+++ SportsBetting/IoCContainer/SportsBetting.IoCContainer.Packages/DataCachePackage.cs
@@ -3,22 +3,35 @@
     using SimpleInjector;
     using SimpleInjector.Packaging;
 
+    using SportsBetting.Data;
     using SportsBetting.Data.Cache;
     using SportsBetting.Data.Cache.General;
     using SportsBetting.Data.Common;
     using SportsBetting.Data.Common.Contracts;
+    using SportsBetting.Data.Contracts;
 
     public sealed class DataCachePackage : IPackage
     {
         public void RegisterServices(Container container)
         {
+            RegisterRepositories(container);
+            RegisterCaches(container);
+        }
+
+        private void RegisterRepositories(Container container)
+        {
+            container.Register<ISportsBettingDbContext, SportsBettingDbContext>(Lifestyle.Singleton);
+            container.Register(typeof(IRepository<>), typeof(MongoRepository<>), Lifestyle.Singleton);
+            container.RegisterDecorator(typeof(IRepository<>), typeof(CacheRepository<>), Lifestyle.Singleton);
             container.Register(typeof(ICacheLoaderRepository<>), typeof(CacheLoaderRepository<>), Lifestyle.Singleton);
+        }
 
-            container.Collection.Register(typeof(ICache <>),
-                typeof(CategoriesCache),
-                typeof(TeamsCache));
-
-            container.Register(typeof(ICache<>), typeof(CacheComposite), Lifestyle.Singleton);
+        private void RegisterCaches(Container container)
+        {
+            container.Register(typeof(ICache<>), typeof(CategoriesCache), Lifestyle.Singleton);
+            container.Register(typeof(ICache<>), typeof(TeamsCache), Lifestyle.Singleton);
+            container.Collection.Register<ICacheInitializer>(typeof(CategoriesCache), typeof(TeamsCache));
+            container.Register<ICacheInitializer, CacheComposite>(Lifestyle.Singleton);
         }
     }
 }
