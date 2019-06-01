2e4822b4ada7b60d36fa6071adb070615db6895b
itplamen@gmail.com <itplamen@gmail.com>
2019-06-02 01:04:59 +0300

    Created teams manager

---
+++ SportsBetting/Feeder/SportsBetting.Feeder.Core/Managers/TeamsManager.cs
@@ -0,0 +1,45 @@
+﻿namespace SportsBetting.Feeder.Core.Managers
+{
+    using System.Linq;
+
+    using SportsBetting.Data.Common.Contracts;
+    using SportsBetting.Data.Models;
+    using SportsBetting.Feeder.Core.Contracts.Managers;
+    using SportsBetting.Feeder.Models;
+
+    public class TeamsManager : ITeamsManager
+    {
+        private readonly IRepository<Team> teamsRepository;
+        private readonly IRepository<Sport> sportsRepository;
+
+        public TeamsManager(IRepository<Team> teamsRepository, IRepository<Sport> sportsRepository)
+        {
+            this.teamsRepository = teamsRepository;
+            this.sportsRepository = sportsRepository;
+        }
+
+        public void Manage(TeamFeedModel feedModel)
+        {
+            Team team = teamsRepository.All(x => x.Key == feedModel.Id).FirstOrDefault();
+
+            if (team == null)
+            {
+                Sport sport = sportsRepository.All(x => x.Key == 1).FirstOrDefault();
+
+                Add(feedModel, sport.Id);
+            }
+        }
+
+        private void Add(TeamFeedModel feedModel, string sportId)
+        {
+            Team team = new Team()
+            {
+                Key = feedModel.Id,
+                Name = feedModel.Name,
+                SportId = sportId
+            };
+
+            teamsRepository.Add(team);
+        }
+    }
+}
