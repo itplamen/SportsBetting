84f5213a7cce37a812f5879bb549271cb2de4a7c
itplamen@gmail.com <itplamen@gmail.com>
2019-06-02 00:22:09 +0300

    Created markets manager

---
+++ SportsBetting/Feeder/SportsBetting.Feeder.Core/Contracts/Managers/IMarketsManager.cs
@@ -0,0 +1,11 @@
+﻿namespace SportsBetting.Feeder.Core.Contracts.Managers
+{
+    using System.Collections.Generic;
+
+    using SportsBetting.Feeder.Models;
+
+    public interface IMarketsManager
+    {
+        void Manage(IEnumerable<MarketFeedModel> feedModels, string matchId);
+    }
+}
---
+++ SportsBetting/Feeder/SportsBetting.Feeder.Core/Managers/MarketsManager.cs
@@ -0,0 +1,45 @@
+﻿namespace SportsBetting.Feeder.Core.Managers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using SportsBetting.Data.Common.Contracts;
+    using SportsBetting.Data.Models;
+    using SportsBetting.Feeder.Core.Contracts.Managers;
+    using SportsBetting.Feeder.Models;
+
+    public class MarketsManager : IMarketsManager
+    {
+        private readonly IRepository<Market> marketsRepository;
+
+        public MarketsManager(IRepository<Market> marketsRepository)
+        {
+            this.marketsRepository = marketsRepository;
+        }
+
+        public void Manage(IEnumerable<MarketFeedModel> feedModels, string matchId)
+        {
+            foreach (var feedModel in feedModels)
+            {
+                Market market = marketsRepository.All(x => x.Key == feedModel.Id).FirstOrDefault();
+
+                if (market == null)
+                {
+                    Add(feedModel, matchId);
+                }
+            }
+        }
+
+        private void Add(MarketFeedModel feedModel, string matchId)
+        {
+            Market market = new Market()
+            {
+                Key = feedModel.Id,
+                Name = feedModel.Name,
+                MatchId = matchId
+            };
+
+            marketsRepository.Add(market);
+        }
+    }
+}
