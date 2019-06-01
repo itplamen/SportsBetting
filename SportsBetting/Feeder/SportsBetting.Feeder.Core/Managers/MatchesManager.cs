801e95970c721ab436c836898012d82a88268f66
itplamen@gmail.com <itplamen@gmail.com>
2019-06-02 00:49:20 +0300

    Created matches mapper

---
+++ SportsBetting/Feeder/SportsBetting.Feeder.Core/Managers/MatchesManager.cs
@@ -0,0 +1,36 @@
+﻿using SportsBetting.Data.Common.Contracts;
+using SportsBetting.Data.Models;
+using SportsBetting.Feeder.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsBetting.Feeder.Core.Managers
+{
+    public class MatchesManager
+    {
+        private readonly IRepository<Match> matchesRepository;
+
+        public MatchesManager(IRepository<Match> matchesRepository)
+        {
+            this.matchesRepository = matchesRepository;
+        }
+
+        public void Manage(MatchFeedModel feedModel)
+        {
+            Match match = matchesRepository.All(x => x.Key == feedModel.Id).FirstOrDefault();
+
+            if (match == null)
+            {
+
+            }
+        }
+
+        private void Add()
+        {
+
+        }
+    }
+}
