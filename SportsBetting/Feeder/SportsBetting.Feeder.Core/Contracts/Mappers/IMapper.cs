a869715e617e495dd73941bdd346ec3c0a02cdb1
itplamen@gmail.com <itplamen@gmail.com>
2019-06-01 23:13:13 +0300

    Created odds mapper

---
+++ SportsBetting/Feeder/SportsBetting.Feeder.Core/Contracts/Mappers/IMapper.cs
@@ -0,0 +1,16 @@
+﻿namespace SportsBetting.Feeder.Core.Contracts.Mappers
+{
+    using System.Collections.Generic;
+
+    using SportsBetting.Data.Models.Base;
+    using SportsBetting.Feeder.Models.Base;
+
+    public interface IMapper<TFrom, To> 
+        where TFrom : BaseFeedModel
+        where To : BaseModel
+    {
+        To Map(TFrom from);
+
+        IEnumerable<To> Map(IEnumerable<TFrom> from);
+    }
+}
