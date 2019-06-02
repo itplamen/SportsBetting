fdd7e23de9b1c0505aba32b18bc3fc560c53e93e
itplamen@gmail.com <itplamen@gmail.com>
2019-06-02 14:43:48 +0300

    Created matches manager

---
+++ SportsBetting/Feeder/SportsBetting.Feeder.Core/Contracts/Managers/IMatchesManager.cs
@@ -0,0 +1,9 @@
+﻿namespace SportsBetting.Feeder.Core.Contracts.Managers
+{
+    using SportsBetting.Feeder.Models;
+
+    public interface IMatchesManager
+    {
+        void Manage(MatchFeedModel feedModel, string categoryId, string tournamentId, string homeTeamId, string awayTeamId);
+    }
+}
--- SportsBetting/Feeder/SportsBetting.Feeder.Core/Managers/MatchesManager.cs
+++ SportsBetting/Feeder/SportsBetting.Feeder.Core/Managers/MatchesManager.cs
@@ -1,36 +1,57 @@
-﻿using SportsBetting.Data.Common.Contracts;
-using SportsBetting.Data.Models;
-using SportsBetting.Feeder.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SportsBetting.Feeder.Core.Managers
+﻿namespace SportsBetting.Feeder.Core.Managers
 {
-    public class MatchesManager
+    using System.Linq;
+
+    using SportsBetting.Data.Common.Contracts;
+    using SportsBetting.Data.Models;
+    using SportsBetting.Feeder.Core.Contracts.Managers;
+    using SportsBetting.Feeder.Core.Contracts.Mappers;
+    using SportsBetting.Feeder.Models;
+
+    public class MatchesManager : IMatchesManager
     {
         private readonly IRepository<Match> matchesRepository;
+        private readonly IMapper<MatchFeedModel, Match> matchesMapper;
 
-        public MatchesManager(IRepository<Match> matchesRepository)
+        public MatchesManager(IRepository<Match> matchesRepository, IMapper<MatchFeedModel, Match> matchesMapper)
         {
             this.matchesRepository = matchesRepository;
+            this.matchesMapper = matchesMapper;
         }
 
-        public void Manage(MatchFeedModel feedModel)
+        public void Manage(MatchFeedModel feedModel, string categoryId, string tournamentId, string homeTeamId, string awayTeamId)
         {
             Match match = matchesRepository.All(x => x.Key == feedModel.Id).FirstOrDefault();
+            Match mappedMatch = matchesMapper.Map(feedModel);
 
             if (match == null)
             {
-
+                Add(mappedMatch, categoryId, tournamentId, homeTeamId, awayTeamId);
+            }
+            else
+            {
+                Update(mappedMatch);
             }
         }
 
-        private void Add()
+        private void Add(Match match, string categoryId, string tournamentId, string homeTeamId, string awayTeamId)
         {
+            match.CategoryId = categoryId;
+            match.TournamentId = tournamentId;
+            match.HomeTeamId = homeTeamId;
+            match.AwayTeamId = awayTeamId;
 
+            matchesRepository.Add(match);
+        }
+
+        private void Update(Match match)
+        {
+            Match matchToUpdate = matchesRepository.All(x => x.Key == match.Key).FirstOrDefault();
+            matchToUpdate.StartTime = match.StartTime;
+            matchToUpdate.Score = match.Score;
+            match.Status = match.Status;
+
+            matchesRepository.Update(matchToUpdate);
         }
     }
 }
