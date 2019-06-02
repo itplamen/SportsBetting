9552b293376aa1f0efe10065b1ead57c75264e4f
itplamen@gmail.com <itplamen@gmail.com>
2019-06-02 15:26:10 +0300

    Registered feeder mappers and managers

--- SportsBetting/IoCContainer/SportsBetting.IoCContainer.Packages/FeederPackage.cs
+++ SportsBetting/IoCContainer/SportsBetting.IoCContainer.Packages/FeederPackage.cs
@@ -3,6 +3,10 @@
     using SimpleInjector;
     using SimpleInjector.Packaging;
 
+    using SportsBetting.Feeder.Core.Contracts.Managers;
+    using SportsBetting.Feeder.Core.Contracts.Mappers;
+    using SportsBetting.Feeder.Core.Managers;
+    using SportsBetting.Feeder.Core.Mappers;
     using SportsBetting.Services.Feeder.Contracts.Factories;
     using SportsBetting.Services.Feeder.Contracts.Providers;
     using SportsBetting.Services.Feeder.Contracts.Services;
@@ -18,9 +22,28 @@
     {
         public void RegisterServices(Container container)
         {
+            RegisterMappers(container);
+            RegisterManagers(container);
             RegisterFactories(container);
             RegisterFeederServices(container);
             RegisterProviders(container);
+        }
+
+        private void RegisterMappers(Container container)
+        {
+            container.Register(typeof(IMapper<,>), typeof(OddsMapper), Lifestyle.Singleton);
+            container.Register(typeof(IMapper<,>), typeof(MatchesMapper), Lifestyle.Singleton);
+        }
+
+        private void RegisterManagers(Container container)
+        {
+            container.Register<ICategoriesManager, CategoriesManager>(Lifestyle.Singleton);
+            container.Register<ITournamentsManager, TournamentsManager>(Lifestyle.Singleton);
+            container.Register<ITeamsManager, TeamsManager>(Lifestyle.Singleton);
+            container.Register<IMatchesManager, MatchesManager>(Lifestyle.Singleton);
+            container.Register<IMarketsManager, MarketsManager>(Lifestyle.Singleton);
+            container.Register<IOddsManager, OddsManager>(Lifestyle.Singleton);
+            container.Register<IFeedManager, FeedManager>(Lifestyle.Singleton);
         }
 
         private void RegisterFactories(Container container)
