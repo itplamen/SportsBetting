2f70755b8dc8b9fe08601885c2840b2cc8ba1cdf
itplamen@gmail.com <itplamen@gmail.com>
2019-06-03 22:57:56 +0300

    Created data IoC package

--- SportsBetting/IoCContainer/SportsBetting.IoCContainer.Packages/DataCachePackage.cs
+++ SportsBetting/IoCContainer/SportsBetting.IoCContainer.Packages/DataCachePackage.cs
@@ -3,31 +3,17 @@
     using SimpleInjector;
     using SimpleInjector.Packaging;
 
-    using SportsBetting.Data;
     using SportsBetting.Data.Cache;
     using SportsBetting.Data.Cache.General;
     using SportsBetting.Data.Common;
     using SportsBetting.Data.Common.Contracts;
-    using SportsBetting.Data.Contracts;
 
     public sealed class DataCachePackage : IPackage
     {
         public void RegisterServices(Container container)
         {
-            RegisterRepositories(container);
-            RegisterCaches(container);
-        }
-
-        private void RegisterRepositories(Container container)
-        {
-            container.Register<ISportsBettingDbContext, SportsBettingDbContext>(Lifestyle.Singleton);
-            container.Register(typeof(IRepository<>), typeof(MongoRepository<>), Lifestyle.Singleton);
-            container.RegisterDecorator(typeof(IRepository<>), typeof(CacheRepository<>), Lifestyle.Singleton);
             container.Register(typeof(ICacheLoaderRepository<>), typeof(CacheLoaderRepository<>), Lifestyle.Singleton);
-        }
 
-        private void RegisterCaches(Container container)
-        {
             container.Register(typeof(ICache<>), typeof(SportsCache), Lifestyle.Singleton);
             container.Register(typeof(ICache<>), typeof(CategoriesCache), Lifestyle.Singleton);
             container.Register(typeof(ICache<>), typeof(TournamentsCache), Lifestyle.Singleton);
@@ -38,7 +24,7 @@
 
             container.Collection.Register<ICacheInitializer>(
                 typeof(SportsCache),
-                typeof(CategoriesCache), 
+                typeof(CategoriesCache),
                 typeof(TournamentsCache),
                 typeof(TeamsCache),
                 typeof(MatchesCache),
---
+++ SportsBetting/IoCContainer/SportsBetting.IoCContainer.Packages/DataPackage.cs
@@ -0,0 +1,20 @@
+﻿namespace SportsBetting.IoCContainer.Packages
+{
+    using SimpleInjector;
+    using SimpleInjector.Packaging;
+
+    using SportsBetting.Data;
+    using SportsBetting.Data.Common;
+    using SportsBetting.Data.Common.Contracts;
+    using SportsBetting.Data.Contracts;
+
+    public sealed class DataPackage : IPackage
+    {
+        public void RegisterServices(Container container)
+        {
+            container.Register<ISportsBettingDbContext, SportsBettingDbContext>(Lifestyle.Singleton);
+            container.Register(typeof(IRepository<>), typeof(MongoRepository<>), Lifestyle.Singleton);
+            container.RegisterDecorator(typeof(IRepository<>), typeof(CacheRepository<>), Lifestyle.Singleton);
+        }
+    }
+}
