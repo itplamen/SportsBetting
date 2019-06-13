98914469b60232c69b3139659ee5c031736708c5
itplamen@gmail.com <itplamen@gmail.com>
2019-06-13 22:56:09 +0300

    Created register view model

---
+++ SportsBetting/Common/SportsBetting.Common/Account/AccountConstants.cs
@@ -0,0 +1,15 @@
+﻿namespace SportsBetting.Common.Account
+{
+    public static class AccountConstants
+    {
+        public const int USERNAME_MAX_LENGTH = 15;
+
+        public const int USERNAME_MIN_LENGTH = 5;
+
+        public const string USERNAME_REG_EX = @"^[a-zA-Z]([/._]?[a-zA-Z0-9]+)+$";
+
+        public const int PASSWORD_MAX_LENGTH = 100;
+
+        public const int PASSWORD_MIN_LENGTH = 6;
+    }
+}
