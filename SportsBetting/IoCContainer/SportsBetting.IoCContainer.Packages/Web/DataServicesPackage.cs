87b91e66bb654558c17052e4b468d324d47fdb29
itplamen@gmail.com <itplamen@gmail.com>
2019-06-14 00:49:44 +0300

    Created web data services package

---
+++ SportsBetting/IoCContainer/SportsBetting.IoCContainer.Packages/Web/DataServicesPackage.cs
@@ -0,0 +1,20 @@
+﻿namespace SportsBetting.IoCContainer.Packages.Web
+{
+    using SimpleInjector;
+    using SimpleInjector.Integration.Web;
+    using SimpleInjector.Packaging;
+
+    using SportsBetting.Services.Data;
+    using SportsBetting.Services.Data.Contracts;
+    using SportsBetting.Services.Utils;
+    using SportsBetting.Services.Utils.Contracts;
+
+    public sealed class DataServicesPackage : IPackage
+    {
+        public void RegisterServices(Container container)
+        {
+            container.Register<IAccountsService, AccountsService>(new WebRequestLifestyle());
+            container.Register<IEncryptionService, EncryptionService>(new WebRequestLifestyle());
+        }
+    }
+}
