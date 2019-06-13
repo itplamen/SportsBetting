1c0792cd0ebe368b5aa797b52b59c5a176cdcca7
itplamen@gmail.com <itplamen@gmail.com>
2019-06-13 23:32:46 +0300

    Created accounts data service

---
+++ SportsBetting/Services/SportsBetting.Services.Data/AccountsService.cs
@@ -0,0 +1,39 @@
+﻿namespace SportsBetting.Services.Data
+{
+    using System.Linq;
+
+    using SportsBetting.Data.Common.Contracts;
+    using SportsBetting.Data.Models;
+    using SportsBetting.Services.Data.Contracts;
+
+    public class AccountsService : IAccountsService
+    {
+        private readonly IRepository<Account> accountsRepository;
+
+        public AccountsService(IRepository<Account> accountsRepository)
+        {
+            this.accountsRepository = accountsRepository;
+        }
+
+        public string Add(Account account)
+        {
+            accountsRepository.Add(account);
+
+            return account.Id;
+        }
+
+        public Account GetByUsername(string username)
+        {
+            Account account = accountsRepository.All(x => x.Username == username).FirstOrDefault();
+
+            return account;
+        }
+
+        public Account GetByEmail(string email)
+        {
+            Account account = accountsRepository.All(x => x.Email == email).FirstOrDefault();
+
+            return account;
+        }
+    }
+}
---
+++ SportsBetting/Services/SportsBetting.Services.Data/Contracts/IAccountsService.cs
@@ -0,0 +1,13 @@
+﻿namespace SportsBetting.Services.Data.Contracts
+{
+    using SportsBetting.Data.Models;
+
+    public interface IAccountsService
+    {
+        string Add(Account account);
+
+        Account GetByUsername(string username);
+
+        Account GetByEmail(string email);
+    }
+}
