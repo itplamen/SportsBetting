dd6c0b59fdb2ef3b3e54c082d76cc43a8aaa36da
itplamen@gmail.com <itplamen@gmail.com>
2019-06-17 23:05:17 +0300

    Created SimpleInjector web config

---
+++ SportsBetting/Clients/SportsBetting.Clients.Web/App_Start/SimpleInjectorConfig.cs
@@ -0,0 +1,35 @@
+﻿namespace SportsBetting.Clients.Web.App_Start
+{
+    using System.Reflection;
+    using System.Web.Mvc;
+
+    using SimpleInjector;
+    using SimpleInjector.Integration.Web.Mvc;
+    using SimpleInjector.Packaging;
+
+    using SportsBetting.IoCContainer.Packages.Web;
+
+    public static class SimpleInjectorConfig
+    {
+        public static void RegisterContainer()
+        {
+            Container container = new Container();
+
+            IPackage[] packages = new IPackage[]
+            {
+                new DataPackage(),
+                new DataServicesPackage()
+            };
+
+            foreach (var package in packages)
+            {
+                package.RegisterServices(container);
+            }
+
+            container.RegisterMvcControllers(Assembly.GetExecutingAssembly());
+            container.Verify();
+
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
+        }
+    }
+}
