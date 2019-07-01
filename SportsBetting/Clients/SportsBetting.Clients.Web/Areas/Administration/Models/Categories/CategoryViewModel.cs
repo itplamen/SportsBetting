50891e845eadda3e739021c9c6beadc033d14c40
itplamen@gmail.com <itplamen@gmail.com>
2019-07-01 23:33:46 +0300

    Created category view model

---
+++ SportsBetting/Clients/SportsBetting.Clients.Web/Areas/Administration/Models/Categories/CategoryViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace SportsBetting.Clients.Web.Areas.Administration.Models.Categories
+{
+    using System;
+
+    public class CategoryViewModel
+    {
+        public string Id { get; set; }
+
+        public int Key { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+
+        public string Name { get; set; }
+
+        public string Sport { get; set; }
+    }
+}
