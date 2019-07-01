47ea2e7abaf93afd2ec32df263c81682fc8787e3
itplamen@gmail.com <itplamen@gmail.com>
2019-07-01 23:34:26 +0300

    Created categories mapper

---
+++ SportsBetting/Clients/SportsBetting.Clients.Web/Areas/Administration/Mappers/CategoriesMapper.cs
@@ -0,0 +1,39 @@
+﻿namespace SportsBetting.Clients.Web.Areas.Administration.Mappers
+{
+    using System.Collections.Generic;
+
+    using SportsBetting.Clients.Web.Areas.Administration.Models.Categories;
+    using SportsBetting.Common.Contracts;
+    using SportsBetting.Data.Models;
+
+    public class CategoriesMapper : IMapper<Category, CategoryViewModel>
+    {
+        public CategoryViewModel Map(Category from)
+        {
+            CategoryViewModel mapped = new CategoryViewModel()
+            {
+                Id = from.Id,
+                Key = from.Key,
+                CreatedOn = from.CreatedOn,
+                ModifiedOn = from.ModifiedOn,
+                IsDeleted = from.IsDeleted,
+                DeletedOn = from.DeletedOn,
+                Name = from.Name
+            };
+
+            return mapped;
+        }
+
+        public IEnumerable<CategoryViewModel> Map(IEnumerable<Category> from)
+        {
+            ICollection<CategoryViewModel> mapped = new List<CategoryViewModel>();
+
+            foreach (var category in from)
+            {
+                mapped.Add(Map(category));
+            }
+
+            return mapped;
+        }
+    }
+}
