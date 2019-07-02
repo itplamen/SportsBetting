ae60af7f5724644c8334381e836957266e47bc49
itplamen@gmail.com <itplamen@gmail.com>
2019-07-03 00:51:05 +0300

    Created tournaments controller

---
+++ SportsBetting/Clients/SportsBetting.Clients.Web/Areas/Administration/Controllers/TournamentsController.cs
@@ -0,0 +1,29 @@
+﻿namespace SportsBetting.Clients.Web.Areas.Administration.Controllers
+{
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+
+    using AutoMapper;
+
+    using SportsBetting.Clients.Web.Areas.Administration.Models.Tournaments;
+    using SportsBetting.Data.Models;
+    using SportsBetting.Services.Data.Contracts;
+
+    public class TournamentsController : Controller
+    {
+        private readonly ITournamentsService tournamentsService;
+
+        public TournamentsController(ITournamentsService tournamentsService)
+        {
+            this.tournamentsService = tournamentsService;
+        }
+
+        public ActionResult Index()
+        {
+            IEnumerable<Tournament> tournaments = tournamentsService.All();
+            IEnumerable<TournamentViewModel> tournamentViewModels = Mapper.Map<IEnumerable<TournamentViewModel>>(tournaments);
+
+            return View(tournamentViewModels);
+        }
+    }
+}
