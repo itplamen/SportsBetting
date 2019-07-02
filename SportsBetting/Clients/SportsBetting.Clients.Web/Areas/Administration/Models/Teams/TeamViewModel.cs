777d686834fd394d1454e85bddaec47b051b8f3e
itplamen@gmail.com <itplamen@gmail.com>
2019-07-03 01:05:08 +0300

    Created team view model

---
+++ SportsBetting/Clients/SportsBetting.Clients.Web/Areas/Administration/Models/Teams/TeamViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SportsBetting.Clients.Web.Areas.Administration.Models.Teams
+{
+    using SportsBetting.Clients.Web.Areas.Administration.Models.Base;
+    using SportsBetting.Clients.Web.Mapping;
+    using SportsBetting.Data.Models;
+
+    public class TeamViewModel : BaseViewModel, IMapFrom<Team>
+    {
+        public string Name { get; set; }
+
+        public string SportId { get; set; }
+    }
+}
