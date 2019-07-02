0f547f852724820b9751ddbe1e38e0dd27954378
itplamen@gmail.com <itplamen@gmail.com>
2019-07-03 00:11:14 +0300

    Created tournament view model

---
+++ SportsBetting/Clients/SportsBetting.Clients.Web/Areas/Administration/Models/Categories/TournamentViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace SportsBetting.Clients.Web.Areas.Administration.Models.Categories
+{
+    using System;
+
+    public class TournamentViewModel
+    {
+        public string Id { get; set; }
+
+        public int Key { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+
+        public string Name { get; set; }
+
+        public string CategoryId { get; set; }
+    }
+}
