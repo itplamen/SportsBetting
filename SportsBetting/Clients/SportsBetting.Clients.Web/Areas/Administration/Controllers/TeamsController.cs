ca53c523862a3da6df590a518bc98fc89c4b5f6c
itplamen@gmail.com <itplamen@gmail.com>
2019-07-03 01:06:32 +0300

    Created teams MVC controller

---
+++ SportsBetting/Clients/SportsBetting.Clients.Web/Areas/Administration/Controllers/TeamsController.cs
@@ -0,0 +1,29 @@
+﻿namespace SportsBetting.Clients.Web.Areas.Administration.Controllers
+{
+    using System.Collections.Generic;
+    using System.Web.Mvc;
+
+    using AutoMapper;
+
+    using SportsBetting.Clients.Web.Areas.Administration.Models.Teams;
+    using SportsBetting.Data.Models;
+    using SportsBetting.Services.Data.Contracts;
+
+    public class TeamsController : Controller
+    {
+        private readonly ITeamsService teamsService;
+
+        public TeamsController(ITeamsService teamsService)
+        {
+            this.teamsService = teamsService;
+        }
+
+        public ActionResult Index()
+        {
+            IEnumerable<Team> teams = teamsService.All();
+            IEnumerable<TeamViewModel> teamViewModels = Mapper.Map<IEnumerable<TeamViewModel>>(teams);
+
+            return View(teamViewModels);
+        }
+    }
+}
