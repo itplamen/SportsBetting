ec8e636b15195d225680857efc0e0e75cfb19c90
itplamen@gmail.com <itplamen@gmail.com>
2019-07-05 01:00:11 +0300

    Created game view model and index view

---
+++ SportsBetting/Clients/SportsBetting.Clients.Web/Models/Home/GameViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace SportsBetting.Clients.Web.Models.Home
+{
+    using System;
+
+    using SportsBetting.Clients.Web.Mapping;
+    using SportsBetting.Data.Models;
+
+    public class GameViewModel : IMapFrom<Match>
+    {
+        public string Id { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public string Score { get; set; }
+
+        public string Category { get; set; }
+
+        public string Tournament { get; set; }
+
+        public string HomeTeam { get; set; }
+
+        public string AwayTeam { get; set; }
+    }
+}
