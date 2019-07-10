0ada815bc2c772947f67b71682409e391b511248
itplamen@gmail.com <itplamen@gmail.com>
2019-07-10 23:31:43 +0300

    Updated web caches' lifestyles

--- SportsBetting/IoCContainer/SportsBetting.IoCContainer.Packages/Web/DataCachePackage.cs
+++ SportsBetting/IoCContainer/SportsBetting.IoCContainer.Packages/Web/DataCachePackage.cs
@@ -20,16 +20,14 @@
             container.Register(typeof(ICache<>), typeof(MarketsCache), new WebRequestLifestyle());
             container.Register(typeof(ICache<>), typeof(OddsCache), new WebRequestLifestyle());
 
-            container.Collection.Register<ICacheInitializer>(
-                typeof(SportsCache),
-                typeof(CategoriesCache),
-                typeof(TournamentsCache),
-                typeof(TeamsCache),
-                typeof(MatchesCache),
-                typeof(MarketsCache),
-                typeof(OddsCache));
-
             container.Register<ICacheInitializer, CacheComposite>(new WebRequestLifestyle());
+            container.Collection.Append<ICacheInitializer, SportsCache>(new WebRequestLifestyle());
+            container.Collection.Append<ICacheInitializer, CategoriesCache>(new WebRequestLifestyle());
+            container.Collection.Append<ICacheInitializer, TournamentsCache>(new WebRequestLifestyle());
+            container.Collection.Append<ICacheInitializer, TeamsCache>(new WebRequestLifestyle());
+            container.Collection.Append<ICacheInitializer, MatchesCache>(new WebRequestLifestyle());
+            container.Collection.Append<ICacheInitializer, MarketsCache>(new WebRequestLifestyle());
+            container.Collection.Append<ICacheInitializer, OddsCache>(new WebRequestLifestyle());
         }
     }
 }
