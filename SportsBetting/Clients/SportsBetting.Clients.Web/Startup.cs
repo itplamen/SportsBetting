2cfea174ca35bb5939dd66045bdc3ad16eb2e5ab
itplamen@gmail.com <itplamen@gmail.com>
2019-07-29 23:38:57 +0300

    Added OWIN Startup class

---
+++ SportsBetting/Clients/SportsBetting.Clients.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SportsBetting.Clients.Web.Startup))]
+namespace SportsBetting.Clients.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
