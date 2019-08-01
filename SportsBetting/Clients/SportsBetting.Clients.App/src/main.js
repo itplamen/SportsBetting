1a68107199b22ca550fdb536a57fb148ed715600
itplamen@gmail.com <itplamen@gmail.com>
2019-08-02 01:03:59 +0300

    Installed bootstrap-vue package

--- SportsBetting/Clients/SportsBetting.Clients.App/src/main.js
+++ SportsBetting/Clients/SportsBetting.Clients.App/src/main.js
@@ -1,6 +1,12 @@
-import Vue from 'vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import Vue from 'vue'
 import router from './router'
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
