e4abd547cb45822582496a8529fa926bd733a5b4
itplamen@gmail.com <itplamen@gmail.com>
2019-08-01 21:46:56 +0300

    Created Api UpcomingGamesController and GameResponseModel

---
+++ SportsBetting/Server/SportsBetting.Server.Api/Controllers/UpcomingGamesController.cs
@@ -0,0 +1,31 @@
+﻿namespace SportsBetting.Server.Api.Controllers
+{
+    using System.Collections.Generic;
+    using System.Web.Http;
+
+    using AutoMapper;
+
+    using SportsBetting.Handlers.Queries.Contracts;
+    using SportsBetting.Handlers.Queries.Matches;
+    using SportsBetting.Server.Api.Models.UpcomingGames;
+
+    public class UpcomingGamesController : ApiController
+    {
+        private readonly IQueryHandler<UpcomingMatchesQuery, IEnumerable<UpcomingMatchesResult>> upcomingMatchesHandler;
+
+        public UpcomingGamesController(IQueryHandler<UpcomingMatchesQuery, IEnumerable<UpcomingMatchesResult>> upcomingMatchesHandler)
+        {
+            this.upcomingMatchesHandler = upcomingMatchesHandler;
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            UpcomingMatchesQuery query = new UpcomingMatchesQuery() { Take = 20 };
+            IEnumerable<UpcomingMatchesResult> upcomingMatches = upcomingMatchesHandler.Handle(query);
+            IEnumerable<GameResponseModel> response = Mapper.Map<IEnumerable<GameResponseModel>>(upcomingMatches);
+
+            return Ok(response);
+        }
+    }
+}
---
+++ SportsBetting/Server/SportsBetting.Server.Api/Models/UpcomingGames/GameResponseModel.cs
@@ -0,0 +1,24 @@
+﻿namespace SportsBetting.Server.Api.Models.UpcomingGames
+{
+    using System;
+
+    using SportsBetting.Common.Infrastructure.Mapping;
+    using SportsBetting.Handlers.Queries.Matches;
+
+    public class GameResponseModel : IMapFrom<UpcomingMatchesResult>
+    {
+        public string Id { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public string Score { get; set; }
+
+        public string Category { get; set; }
+
+        public string Tournament { get; set; }
+
+        public string HomeTeam { get; set; }
+
+        public string AwayTeam { get; set; }
+    }
+}
