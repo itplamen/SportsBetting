440bc4d4b3d74eece123a8181d4d645d4f351d8f
itplamen@gmail.com <itplamen@gmail.com>
2019-08-08 23:32:13 +0300

    Deleted unused components and views

--- SportsBetting/Clients/SportsBetting.Clients.App/src/App.vue
+++ SportsBetting/Clients/SportsBetting.Clients.App/src/App.vue
@@ -5,7 +5,6 @@
       <router-link to="/about">About</router-link> |
       <router-link to="" v-b-modal.RegisterModal>Register</router-link>
     </div>
-    <router-view/>
     <UpcomingGames v-if="isHomePage === true" />
     <Register />
   </div>
