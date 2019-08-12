b308e4b8499512cf291a8f60c956bbbcaf24cc7b
itplamen@gmail.com <itplamen@gmail.com>
2019-08-12 23:59:42 +0300

    Created SportsBetting.Feeder.WinService project

---
+++ SportsBetting/Feeder/SportsBetting.Feeder.WinService/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SportsBetting.Feeder.WinService
+{
+    using System.ServiceProcess;
+
+    static class Program
+    {
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new SportsBettingService()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ SportsBetting/Feeder/SportsBetting.Feeder.WinService/SportsBettingService.cs
@@ -0,0 +1,33 @@
+﻿namespace SportsBetting.Feeder.WinService
+{
+    using System.ServiceProcess;
+
+    using SportsBetting.Feeder.Bootstrap;
+
+    public partial class SportsBettingService : ServiceBase
+    {
+        private readonly FeederBootstrapper bootstrapper;
+
+        public SportsBettingService()
+        {
+            InitializeComponent();
+
+            bootstrapper = new FeederBootstrapper();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            bootstrapper.Start();
+        }
+
+        protected override void OnStop()
+        {
+            bootstrapper.Stop();
+        }
+
+        protected override void OnShutdown()
+        {
+            this.OnStop();
+        }
+    }
+}
