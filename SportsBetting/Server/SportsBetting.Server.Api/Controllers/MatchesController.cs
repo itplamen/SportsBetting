a8a8ffee063a50ea8fac5d89a84f9d29d5c04134
itplamen@gmail.com <itplamen@gmail.com>
2019-08-14 22:04:49 +0300

    Updated EsportsController to MatchesController

--- SportsBetting/Server/SportsBetting.Server.Api/Controllers/MatchesController.cs
+++ SportsBetting/Server/SportsBetting.Server.Api/Controllers/MatchesController.cs
@@ -1,94 +1,68 @@
 ﻿namespace SportsBetting.Server.Api.Controllers
 {
     using System.Collections.Generic;
-    using System.Linq;
     using System.Web.Http;
     using System.Web.Http.Cors;
 
     using AutoMapper;
 
-    using SportsBetting.Data.Models;
-    using SportsBetting.Handlers.Queries.Common;
+    using SportsBetting.Handlers.Queries.Common.Results;
     using SportsBetting.Handlers.Queries.Contracts;
-    using SportsBetting.Server.Api.Models.Markets;
+    using SportsBetting.Handlers.Queries.Matches;
     using SportsBetting.Server.Api.Models.Matches;
-    using SportsBetting.Server.Api.Models.Odds;
 
     [EnableCors("*", "*", "*")]
     public class MatchesController : ApiController
     {
-        private readonly IQueryHandler<EntitiesByIdQuery<Team>, IEnumerable<Team>> teamByIdHandler;
-        private readonly IQueryHandler<EntitiesByIdQuery<Match>, IEnumerable<Match>> matchByIdHandler;
-        private readonly IQueryHandler<EntitiesByIdQuery<Category>, IEnumerable<Category>> categoryByIdHandler;
-        private readonly IQueryHandler<EntitiesByIdQuery<Tournament>, IEnumerable<Tournament>> tournamentByIdHandler;
-        private readonly IQueryHandler<EntitiesByIdQuery<Odd>, IEnumerable<Odd>> oddsByMarketIdHandler;
-        private readonly IQueryHandler<EntitiesByIdQuery<Market>, IEnumerable<Market>> marketsByMatchIdHandler;
+        private readonly IQueryHandler<MatchByIdQuery, MatchResult> matchesByIdHandler;
+        private readonly IQueryHandler<AllMatchesQuery, IEnumerable<MatchResult>> allMatchesHandler;
 
         public MatchesController(
-            IQueryHandler<EntitiesByIdQuery<Team>, IEnumerable<Team>> teamByIdHandler,
-            IQueryHandler<EntitiesByIdQuery<Match>, IEnumerable<Match>> matchByIdHandler,
-            IQueryHandler<EntitiesByIdQuery<Category>, IEnumerable<Category>> categoryByIdHandler,
-            IQueryHandler<EntitiesByIdQuery<Tournament>, IEnumerable<Tournament>> tournamentByIdHandler,
-            IQueryHandler<EntitiesByIdQuery<Odd>, IEnumerable<Odd>> oddsByMarketIdHandler,
-            IQueryHandler<EntitiesByIdQuery<Market>, IEnumerable<Market>> marketsByMatchIdHandler)
+            IQueryHandler<MatchByIdQuery, MatchResult> matchesByIdHandler,
+            IQueryHandler<AllMatchesQuery, IEnumerable<MatchResult>> allMatchesHandler)
         {
-            this.teamByIdHandler = teamByIdHandler;
-            this.matchByIdHandler = matchByIdHandler;
-            this.categoryByIdHandler = categoryByIdHandler;
-            this.tournamentByIdHandler = tournamentByIdHandler;
-            this.oddsByMarketIdHandler = oddsByMarketIdHandler;
-            this.marketsByMatchIdHandler = marketsByMatchIdHandler;
+            this.matchesByIdHandler = matchesByIdHandler;
+            this.allMatchesHandler = allMatchesHandler;
+        }
+
+        [HttpGet]
+        public IHttpActionResult All(int take)
+        {
+            if (take <= 0)
+            {
+                ModelState.AddModelError("Take", "Invalid request!");
+
+                return BadRequest(ModelState);
+            }
+
+            AllMatchesQuery query = new AllMatchesQuery(take);
+            IEnumerable<MatchResult> matches = allMatchesHandler.Handle(query);
+            IEnumerable<MatchResponseModel> responseModel = Mapper.Map<IEnumerable<MatchResponseModel>>(matches);
+
+            return Ok(responseModel);
         }
 
         [HttpGet]
         public IHttpActionResult Get(string id)
         {
-            EntitiesByIdQuery<Match> matchByIdQuery = new EntitiesByIdQuery<Match>(new List<string>() { id });
-            Match match = matchByIdHandler.Handle(matchByIdQuery).FirstOrDefault();
+            if (string.IsNullOrEmpty(id))
+            {
+                ModelState.AddModelError("Id", "Invalid request!");
 
-            if (match == null)
+                return BadRequest(ModelState);
+            }
+
+            MatchByIdQuery query = new MatchByIdQuery(id);
+            MatchResult result = matchesByIdHandler.Handle(query);
+
+            if (result == null)
             {
                 return NotFound();
             }
 
-            EntitiesByIdQuery<Category> categoryByIdQuery = new EntitiesByIdQuery<Category>(new List<string>() { match.CategoryId });
-            Category category = categoryByIdHandler.Handle(categoryByIdQuery).FirstOrDefault();
+            MatchResponseModel responseModel = Mapper.Map<MatchResponseModel>(result);
 
-            EntitiesByIdQuery<Tournament> tournamentByIdQuery = new EntitiesByIdQuery<Tournament>(new List<string>() { match.TournamentId });
-            Tournament tournament = tournamentByIdHandler.Handle(tournamentByIdQuery).FirstOrDefault();
-
-            EntitiesByIdQuery<Team> homeTeamByIdQuery = new EntitiesByIdQuery<Team>(new List<string>() { match.HomeTeamId });
-            Team homeTeam = teamByIdHandler.Handle(homeTeamByIdQuery).FirstOrDefault();
-
-            EntitiesByIdQuery<Team> awayTeamByIdQuery = new EntitiesByIdQuery<Team>(new List<string>() { match.AwayTeamId });
-            Team awayTeam = teamByIdHandler.Handle(awayTeamByIdQuery).FirstOrDefault();
-
-            IEnumerable<string> matchIds = new List<string>() { match.Id };
-            EntitiesByIdQuery<Market> marketsByMatchIdQuery = new EntitiesByIdQuery<Market>(matchIds, x => matchIds.Contains(x.MatchId));
-            IEnumerable<Market> markets = marketsByMatchIdHandler.Handle(marketsByMatchIdQuery);
-
-            ICollection<MarketResponseModel> marketsResponseModel = new List<MarketResponseModel>();
-
-            foreach (var market in markets)
-            {
-                IEnumerable<string> marketIds = new List<string>() { market.Id };
-                EntitiesByIdQuery<Odd> oddsByMarketIdQuery = new EntitiesByIdQuery<Odd>(marketIds, x => marketIds.Contains(x.MarketId));
-                IEnumerable<Odd> odds = oddsByMarketIdHandler.Handle(oddsByMarketIdQuery);
-
-                MarketResponseModel marketResponseModel = Mapper.Map<MarketResponseModel>(market);
-                marketResponseModel.Odds = Mapper.Map<IEnumerable<OddResponseModel>>(odds);
-
-                marketsResponseModel.Add(marketResponseModel);
-            }
-
-            MatchResponseModel matchResponseModel = Mapper.Map<MatchResponseModel>(match);
-            matchResponseModel.Category = category.Name;
-            matchResponseModel.Tournament = tournament.Name;
-            matchResponseModel.HomeTeam = homeTeam.Name;
-            matchResponseModel.AwayTeam = awayTeam.Name;
-            matchResponseModel.Markets = marketsResponseModel;
-
-            return Ok(matchResponseModel);
+            return Ok(responseModel);
         }
     }
 }
