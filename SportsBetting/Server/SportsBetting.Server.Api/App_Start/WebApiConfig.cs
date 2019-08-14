9a618c14fbb40033e34cec6452f94ba5f131a968
itplamen@gmail.com <itplamen@gmail.com>
2019-08-14 22:03:35 +0300

    Added new Web API route

--- SportsBetting/Server/SportsBetting.Server.Api/App_Start/WebApiConfig.cs
+++ SportsBetting/Server/SportsBetting.Server.Api/App_Start/WebApiConfig.cs
@@ -1,6 +1,8 @@
 ﻿namespace SportsBetting.Server.Api
 {
+    using System.Net.Http;
     using System.Web.Http;
+    using System.Web.Http.Routing;
 
     public static class WebApiConfig
     {
@@ -13,6 +15,13 @@
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "AllMatches",
+                routeTemplate: "api/{controller}/{action}/{take}",
+                defaults: new { controller = "Matches", action = "All" },
+                constraints: new { take = @"\d+", httpMethod = new HttpMethodConstraint(HttpMethod.Get) }
             );
         }
     }
