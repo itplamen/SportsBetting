8f0b16b54f17de2fd87f890bac8b78a190098c4a
itplamen@gmail.com <itplamen@gmail.com>
2019-08-20 19:30:31 +0300

    Added logging for all synchronizers

--- SportsBetting/Feeder/SportsBetting.Feeder.Core/LoggingSynchronizer.cs
+++ SportsBetting/Feeder/SportsBetting.Feeder.Core/LoggingSynchronizer.cs
@@ -2,15 +2,22 @@
 {
     using System;
 
+    using SportsBetting.Common.Contracts;
     using SportsBetting.Feeder.Core.Contracts;
     
     public class LoggingSynchronizer : ISynchronizer
     {
+        private const string LOGGER = nameof(LoggingSynchronizer);
+
+        private readonly ILogger logger;
+        private readonly string synchronizerName;
         private readonly ISynchronizer decoratedSynchronizer;
 
-        public LoggingSynchronizer(ISynchronizer decoratedSynchronizer)
+        public LoggingSynchronizer(ISynchronizer decoratedSynchronizer, ILoggerFactory loggerFactory)
         {
             this.decoratedSynchronizer = decoratedSynchronizer;
+            this.synchronizerName = decoratedSynchronizer.GetType().Name;
+            this.logger = loggerFactory.Create($"{logger}.txt", "Synchronizers");
         }
 
         public void Sync()
@@ -21,7 +28,7 @@
             }
             catch (Exception ex)
             {
-                // TODO: Log
+                logger.LogError($"{synchronizerName} failed", ex);
             }
         }
 
