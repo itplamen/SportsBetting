66a463643bca06ac32fee24db7e8a5e3176ea06c
itplamen@gmail.com <itplamen@gmail.com>
2019-08-21 01:03:44 +0300

    Created SimpleInjectorHubActivator

---
+++ SportsBetting/Server/SportsBetting.Server.Api/Hubs/SimpleInjectorHubActivator.cs
@@ -0,0 +1,21 @@
+﻿namespace SportsBetting.Server.Api.Hubs
+{
+    using Microsoft.AspNet.SignalR.Hubs;
+
+    using SimpleInjector;
+
+    public class SimpleInjectorHubActivator : IHubActivator
+    {
+        private readonly Container container;
+
+        public SimpleInjectorHubActivator(Container container)
+        {
+            this.container = container;
+        }
+
+        public IHub Create(HubDescriptor descriptor)
+        {
+            return (IHub)container.GetInstance(descriptor.HubType);
+        }
+    }
+}
