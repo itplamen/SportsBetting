92bf5129d131a0df8ce014487882f2a433c913f5
itplamen@gmail.com <itplamen@gmail.com>
2019-10-13 12:02:27 +0300

    Created common validation result model

---
+++ SportsBetting/Common/SportsBetting.Common/Validation/ValidationResult.cs
@@ -0,0 +1,21 @@
+﻿namespace SportsBetting.Common.Validation
+{
+    public class ValidationResult
+    {
+        public ValidationResult(string errorKey, string errorMessage)
+        {
+            ErrorKey = errorKey;
+            ErrorMessage = errorMessage;
+        }
+
+        public ValidationResult()
+        {
+        }
+
+        public string ErrorKey { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        public bool HasErrors => !string.IsNullOrEmpty(ErrorKey) && !string.IsNullOrEmpty(ErrorMessage);
+    }
+}
