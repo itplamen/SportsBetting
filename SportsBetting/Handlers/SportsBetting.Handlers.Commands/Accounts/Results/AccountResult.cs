e8525b3129189afe88e81358274189279ed6973c
itplamen@gmail.com <itplamen@gmail.com>
2019-10-14 01:53:31 +0300

    Created account result

---
+++ SportsBetting/Handlers/SportsBetting.Handlers.Commands/Accounts/Results/AccountResult.cs
@@ -0,0 +1,15 @@
+﻿namespace SportsBetting.Handlers.Commands.Accounts.Results
+{
+    using SportsBetting.Common.Results;
+    using SportsBetting.Data.Models;
+
+    public class AccountResult : ValidationResult
+    {
+        public AccountResult(Account account)
+        {
+            Account = account;
+        }
+
+        public Account Account { get; set; }
+    }
+}
