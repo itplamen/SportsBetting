79c62a27f188fb87b76668e044610eb143dde0f5
itplamen@gmail.com <itplamen@gmail.com>
2019-10-13 16:51:32 +0300

    Created base abstract account request model in order to extract common logic for registering and login accounts

--- SportsBetting/Server/SportsBetting.Server.Api/Models/Account/RegisterRequestModel.cs
+++ SportsBetting/Server/SportsBetting.Server.Api/Models/Account/RegisterRequestModel.cs
@@ -2,29 +2,11 @@
 {
     using System.ComponentModel.DataAnnotations;
 
-    using SportsBetting.Common.Constants;
     using SportsBetting.Common.Infrastructure.Mapping;
     using SportsBetting.Handlers.Commands.Accounts;
 
-    public class RegisterRequestModel : IMapTo<CreateAccountCommand>
+    public class RegisterRequestModel : AccountRequestModel, IMapTo<CreateAccountCommand>
     {
-        [Required(ErrorMessage = "{0} is required")]
-        [StringLength(
-               AccountValidationConstants.USERNAME_MAX_LENGTH,
-               MinimumLength = AccountValidationConstants.USERNAME_MIN_LENGTH,
-               ErrorMessage = "{0} must be between {2} and {1} characters long")]
-        [RegularExpression(
-               AccountValidationConstants.USERNAME_REG_EX,
-               ErrorMessage = "{0} is in invalid format")]
-        public string Username { get; set; }
-
-        [Required(ErrorMessage = "{0} is required")]
-        [StringLength(
-            AccountValidationConstants.PASSWORD_MAX_LENGTH,
-            MinimumLength = AccountValidationConstants.PASSWORD_MIN_LENGTH,
-            ErrorMessage = "{0} must be between {2} and {1} characters long")]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
 
         [Required(ErrorMessage = "Confirm your password")]
         [DataType(DataType.Password)]
