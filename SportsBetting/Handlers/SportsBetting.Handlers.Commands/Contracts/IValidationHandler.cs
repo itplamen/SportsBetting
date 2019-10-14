5abee07fc1325e66722226428245e28a35a69643
itplamen@gmail.com <itplamen@gmail.com>
2019-10-14 21:36:29 +0300

    Created IValidationHandler contract

---
+++ SportsBetting/Handlers/SportsBetting.Handlers.Commands/Contracts/IValidationHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace SportsBetting.Handlers.Commands.Contracts
+{
+    using SportsBetting.Common.Results;
+
+    public interface IValidationHandler<in TCommand> 
+        where TCommand : ICommand
+    {
+        ValidationResult Validate(TCommand command);
+    }
+}
