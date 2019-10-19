28995c95c785cfbaf3b63e3fb8b8b984ef639b5a
itplamen@gmail.com <itplamen@gmail.com>
2019-10-19 20:43:54 +0300

    Created model state extension method

---
+++ SportsBetting/Server/SportsBetting.Server.Api/Extensions/ModelStateExtension.cs
@@ -0,0 +1,18 @@
+﻿namespace SportsBetting.Server.Api.Extensions
+{
+    using System.Collections.Generic;
+    using System.Web.Http.ModelBinding;
+
+    using SportsBetting.Common.Results;
+
+    public static class ModelStateExtension
+    {
+        public static void AddModelErrors(this ModelStateDictionary dictionary, IEnumerable<ValidationResult> validations)
+        {
+            foreach (var validation in validations)
+            {
+                dictionary.AddModelError(validation.ErrorKey, validation.ErrorMessage);
+            }
+        }
+    }
+}
