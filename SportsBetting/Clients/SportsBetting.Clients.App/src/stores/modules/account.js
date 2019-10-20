cd4da7ddc01e42834c8efbcad8133d92af1d26a2
itplamen@gmail.com <itplamen@gmail.com>
2019-10-20 17:31:16 +0300

    Added logic for retrieving account's login status

--- SportsBetting/Clients/SportsBetting.Clients.App/src/stores/modules/account.js
+++ SportsBetting/Clients/SportsBetting.Clients.App/src/stores/modules/account.js
@@ -5,29 +5,46 @@ const state = {
   account: {
     id: '',
     username: '',
-    balance: 0
+    balance: 0,
+    loginToken: '',
+    expiration: ''
   }
 };
 
 const getters = {
-  getAccount: (state) => state.account
+  getAccount: (state) => state.account,
+  isLoggedIn: (state) => {
+    let authentication = localStorage.authentication;
+
+    if (authentication) {
+      let expirationDate = new Date(authentication.split('|')[1]);
+      let dateNow = new Date();
+
+      return expirationDate > dateNow;
+    }
+
+    return false;
+  }
 };
 
 const actions = {
   async registerAccount ({ commit }, request) {
-    const response = await axios.post(endpoints.REGISTER_ACCOUNT, request)
-
-    commit('setAccount', response.data)
-  },
-  async fetchAccount ({ commit }) {
-    const response = await axios.get('http://localhost:64399/api/Account')
-
-    commit('setAccount', response)
+    const response = await axios.post(endpoints.REGISTER_ACCOUNT, request);
+    
+    commit('setAccount', response.data);    
   }
 };
 
 const mutations = {
-  setAccount: (state, account) => (state.account = account)
+  setAccount: (state, account) => {
+    state.account.id = account.Id;
+    state.account.username = account.Username;
+    state.account.balance = account.Balance;
+    state.account.loginToken = account.LoginToken;
+    state.account.expiration = account.Expiration;
+
+    localStorage.authentication = `${state.account.loginToken}|${state.account.expiration}`;
+  }
 };
 
 export default {
