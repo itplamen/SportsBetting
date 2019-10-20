a74f21db0784fdae47d654e38849508ab90a0e86
itplamen@gmail.com <itplamen@gmail.com>
2019-10-20 11:51:18 +0300

    Created mapper extensions

---
+++ SportsBetting/Server/SportsBetting.Server.Api/Extensions/MapperExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace SportsBetting.Server.Api.Extensions
+{
+    using AutoMapper;
+
+    public static class MapperExtensions
+    {
+        public static TDestination Map<TSource, TDestination>(this TDestination destination, TSource source)
+        {
+            return Mapper.Map(source, destination);
+        }
+    }
+}
