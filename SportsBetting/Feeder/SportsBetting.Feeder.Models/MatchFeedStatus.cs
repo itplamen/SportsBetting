5b3f6676d92814ae6403df0a84cce71c62b5cc7a
itplamen@gmail.com <itplamen@gmail.com>
2019-10-22 22:28:30 +0300

    Uninstalled Newtonsoft.Json v.12.0. from SportsBetting.Feeder.Models project and removed some invalid match statuses

--- SportsBetting/Feeder/SportsBetting.Feeder.Models/MatchFeedStatus.cs
+++ SportsBetting/Feeder/SportsBetting.Feeder.Models/MatchFeedStatus.cs
@@ -1,35 +1,9 @@
 ﻿namespace SportsBetting.Feeder.Models
 {
-    using System.Runtime.Serialization;
-
-    using Newtonsoft.Json;
-    using Newtonsoft.Json.Converters;
-
-    [JsonConverter(typeof(StringEnumConverter))]
     public enum MatchFeedStatus
     {
-        [EnumMember(Value = "NOT STARTED")]
         NotStarted,
-
-        [EnumMember(Value = "LIVE")]
         InPlay,
-
-        [EnumMember(Value = "ENDED")]
-        Ended,
-
-        [EnumMember(Value = "SUSPENDED")]
-        Suspended,
-
-        [EnumMember(Value = "ABANDONED")]
-        Abandoned,
-
-        [EnumMember(Value = "CLOSED")]
-        Closed,
-
-        [EnumMember(Value = "CANCELLED")]
-        Cancelled,
-
-        [EnumMember(Value = "DELAYED")]
-        Delayed
+        Ended
     }
 }
