36af7e7f4d350d86940f7da883e2e36a2c60b30e
itplamen@gmail.com <itplamen@gmail.com>
2019-10-26 19:25:45 +0300

    Created endpoint for placing bet

---
+++ SportsBetting/Server/SportsBetting.Server.Api/Controllers/BetsController.cs
@@ -0,0 +1,46 @@
+﻿namespace SportsBetting.Server.Api.Controllers
+{
+    using System.Collections.Generic;
+    using System.Web.Http;
+    using System.Web.Http.Cors;
+
+    using AutoMapper;
+
+    using SportsBetting.Common.Results;
+    using SportsBetting.Handlers.Commands.Bets.Commands;
+    using SportsBetting.Handlers.Commands.Contracts;
+    using SportsBetting.Server.Api.Extensions;
+    using SportsBetting.Server.Api.Models.Bets;
+
+    [EnableCors("*", "*", "*")]
+    public class BetsController : ApiController
+    {
+        private readonly ICommandDispatcher commandDispatcher;
+
+        public BetsController(ICommandDispatcher commandDispatcher)
+        {
+            this.commandDispatcher = commandDispatcher;
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post(BetRequestModel requestModel)
+        {
+            if (ModelState.IsValid)
+            {
+                PlaceBetCommand placeBetCommand = Mapper.Map<PlaceBetCommand>(requestModel);
+                IEnumerable<ValidationResult> validations = commandDispatcher.Validate(placeBetCommand);
+
+                ModelState.AddModelErrors(validations);
+
+                if (ModelState.IsValid)
+                {
+                    commandDispatcher.Dispatch(placeBetCommand);
+
+                    return Ok();
+                }
+            }
+
+            return BadRequest(ModelState);
+        }
+    }
+}
---
+++ SportsBetting/Server/SportsBetting.Server.Api/Models/Bets/BetRequestModel.cs
@@ -0,0 +1,19 @@
+﻿namespace SportsBetting.Server.Api.Models.Bets
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using SportsBetting.Common.Infrastructure.Mapping;
+    using SportsBetting.Handlers.Commands.Bets.Commands;
+
+    public class BetRequestModel : IMapTo<PlaceBetCommand>
+    {
+        [Required(ErrorMessage = "{0} is required")]
+        public decimal Stake { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        public string OddId { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        public string AccountId { get; set; }
+    }
+}
