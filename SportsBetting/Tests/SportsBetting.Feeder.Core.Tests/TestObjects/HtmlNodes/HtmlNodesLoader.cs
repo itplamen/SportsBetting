1b898d6deab331968bd39b16a5bc07fee2b6a17c
itplamen@gmail.com <itplamen@gmail.com>
2019-10-29 23:23:43 +0200

    Created TestObjectFactoryCommonHtmlNodes class

---
+++ SportsBetting/Tests/SportsBetting.Feeder.Core.Tests/TestObjects/HtmlNodes/HtmlNodesLoader.cs
@@ -0,0 +1,19 @@
+﻿namespace SportsBetting.Feeder.Core.Tests.TestObjects.HtmlNodes
+{
+    using System.Text.RegularExpressions;
+
+    using HtmlAgilityPack;
+
+    public static class HtmlNodesLoader
+    {
+        public static HtmlNode Load(string html)
+        {
+            html = Regex.Replace(html, @"\s*(<[^>]+>)\s*", "$1", RegexOptions.Singleline);
+
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(html);
+
+            return document.DocumentNode.FirstChild;
+        }
+    }
+}
