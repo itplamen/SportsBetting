4e23f14ccc9e05a41dcd398876e70ab116e92eb2
itplamen@gmail.com <itplamen@gmail.com>
2019-10-29 23:09:09 +0200

    Created SportsBetting.Feeder.Core.Tests project

---
+++ SportsBetting/Tests/SportsBetting.Feeder.Core.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SportsBetting.Feeder.Core.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SportsBetting.Feeder.Core.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3f6d9825-74c2-4273-99ed-33bb5ff43d12")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
