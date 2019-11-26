ba8a3ed50c749153564dda200002bd294b9c6c23
itplamen@gmail.com <itplamen@gmail.com>
2019-11-27 00:05:06 +0200

    Created unit tests for WebPagesService

---
+++ SportsBetting/Tests/SportsBetting.Feeder.Core.Tests/ServicesTests/WebPagesServiceTests.cs
@@ -0,0 +1,64 @@
+﻿namespace SportsBetting.Feeder.Core.Tests.ServicesTests
+{
+    using System.Linq;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using SportsBetting.Feeder.Core.Contracts.Services;
+    using SportsBetting.Feeder.Core.Services;
+    using SportsBetting.Feeder.Core.Tests.TestObjects.HtmlNodes.Stubs;
+
+    [TestClass]
+    public class WebPagesServiceTests
+    {
+        private WebDriverStub webDriver;
+        private IWebPagesService<WebDriverStub> webPagesService;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            webDriver = new WebDriverStub();
+            webPagesService = new WebPagesService<WebDriverStub>();
+        }
+
+        [TestMethod]
+        public void LoadShouldReturnFalseWhenUrlAndWaitForElementAreNull()
+        {
+            bool isLoaded = webPagesService.Load(webDriver, null, null);
+
+            Assert.IsFalse(isLoaded);
+        }
+
+        [TestMethod]
+        public void LoadShouldReturnFalseWhenWaitForElementIsInvalid()
+        {
+            bool isLoaded = webPagesService.Load(webDriver, "url", "invalidWaitForElement");
+
+            Assert.IsFalse(isLoaded);
+        }
+
+        [TestMethod]
+        public void LoadShouldReturnTrueWhenWaitForElementIsValid()
+        {
+            bool isLoaded = webPagesService.Load(webDriver, "url", "waitForElement");
+
+            Assert.IsTrue(isLoaded);
+        }
+
+        [TestMethod]
+        public void LoadShouldReturnTrueWhenWaitForElementIsInvalidButUrlIsValid()
+        {
+            bool isLoaded = webPagesService.Load(webDriver, "TestUrl", "invalidWaitForElement");
+
+            Assert.IsTrue(isLoaded);
+        }
+
+        [TestMethod]
+        public void ScrollToBottomShouldHasAtleastOneHandledWindows()
+        {
+            webPagesService.ScrollToBottom(webDriver);
+
+            Assert.IsTrue(webDriver.WindowHandles.Any());
+        }
+    }
+}
